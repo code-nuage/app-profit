34b3c61651a67ff66a5bf1a38d07fda9c94cac5d
Eloi Nuage Hariot <eloi.hariot@protonmail.com>
2025-04-08 11:20:45 +0200

    0.0.1 (Initial commit)

---
+++ back/package.lua
@@ -0,0 +1,15 @@
+  return {
+    name = "app-profit",
+    version = "0.0.1",
+    description = "A simple description of my little package.",
+    tags = { "lua", "lit", "luvit" },
+    license = "MIT",
+    author = { name = "Eloi Nuage Hariot", email = "eloi.hariot@protonmail.com" },
+    homepage = "https://github.com/app-profit",
+    dependencies = {},
+    files = {
+      "**.lua",
+      "!test*"
+    }
+  }
+  
